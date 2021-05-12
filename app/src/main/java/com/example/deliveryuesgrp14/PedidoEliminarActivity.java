2a7c3abd9bc846efaba1555d93282b41ed1ad36c
Agustin <ah15035@ues.edu.sv>
2021-05-12 13:31:39 -0600

    Inicializando actividades de pedido

---
+++ app/src/main/java/com/example/deliveryuesgrp14/PedidoEliminarActivity.java
@@ -0,0 +1,14 @@
+package com.example.deliveryuesgrp14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PedidoEliminarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedido_eliminar);
+    }
+}
