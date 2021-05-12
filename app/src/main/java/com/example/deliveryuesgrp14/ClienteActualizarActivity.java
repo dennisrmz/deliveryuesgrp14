668642a3525108c9dea940881a823b37acc737ba
Agustin <ah15035@ues.edu.sv>
2021-05-12 17:02:21 -0600

    Inicializando actividades de Cliente

---
+++ app/src/main/java/com/example/deliveryuesgrp14/ClienteActualizarActivity.java
@@ -0,0 +1,14 @@
+package com.example.deliveryuesgrp14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClienteActualizarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cliente_actualizar);
+    }
+}
